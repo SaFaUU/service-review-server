92422821042e046cdbbdfaf0edf08d969a101783
SaFaUU <thulk283@gmail.com>
2022-11-10 02:11:49 +0600

    Added mongodb connection

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Service Review Server Running!');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1cmhy5v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        app.get('/addtour', (req, res) => {
+            res.send(uri)
+        })
+    }
+    catch {
+
+    }
+
+}
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send("Tour Service Server is running")
 })
 
 app.listen(port, () => {
